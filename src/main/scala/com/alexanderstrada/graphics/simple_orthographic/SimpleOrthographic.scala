ecb7d22aa92198f387147f34f12c422c5c0c1de6
Alexander Strada <astrada2023@gmail.com>
2015-11-25 00:59:09 -0600

    style(SimpleOrthographic): clean up and make more modular

--- src/main/scala/com/alexanderstrada/graphics/simple_orthographic/SimpleOrthographic.scala
+++ src/main/scala/com/alexanderstrada/graphics/simple_orthographic/SimpleOrthographic.scala
@@ -15,62 +15,64 @@ import com.alexanderstrada.space3d.tuple3d.Tuple3dD.Point3dD
  */
 object SimpleOrthographic {
 
-  /**
-   * The value by which top faces' sorting positions are adjusted, to give them priority over front
-   * faces at identical depths. Without this, when a box is in surface contact with a box behind it,
-   * the top face of the front box would sometimes be placed behind the front face of the rear box.
-   */
-  private val topFaceDepthPriorityNudge = 0.00001
 
-  /**
-   * The value by which the Z coordinate of a face's origin will be multiplied when its sorting
-   * depth is calculated. Controls the influence of vertical position on sorting order.
-   */
-  private val sortDepthZFactor = 0.1
+  def makeFace(projectedOrigin: Point3dD,
+               width: Double,
+               height: Double,
+               outline: Option[Color],
+               fill: Option[Color],
+               image: Option[Image],
+               sortDepth: Double = 0.0,
+               cam: OrthographicCamera): Face = {
 
-
-  def apply(camera: OrthographicCamera, zoneDrawables: Seq[BoxDrawable]): Seq[Face] = {
 
     implicit def toInt(d: Double): Int = d.round.asInstanceOf[Int]
 
-    def toFaces(r: BoxDrawable): Seq[Face] = {
-      val size = camera.project(r.box.size)
+    val r = Rectangle(projectedOrigin.x,
+                      projectedOrigin.y - projectedOrigin.z,
+                      width,
+                      height)
 
-      val sortOrigin = r.box.vertex(LEFT, BACK, TOP)
-      val sortDepth = sortOrigin.y + (sortOrigin.z * sortDepthZFactor)
+    val adjustedForCamPosition = r -(cam.screen.x, cam.screen.y)
 
-      def makeFace(worldOrigin: Point3dD,
-                   height: Double,
-                   outline: Option[Color],
-                   fill: Option[Color],
-                   image: Option[Image],
-                   sortDepthAdjustment: Double = 0.0): Face = {
+    val compensatedForProjection = adjustedForCamPosition + (y = cam.depthCompensation)
 
-        val projOrigin = camera.project(worldOrigin)
-
-        val rect = (Rectangle(projOrigin.x, projOrigin.y - projOrigin.z, size.x, height)
-                    -(camera.screen.x, camera.screen.y) // Adjust for camera position.
-                    + (y = camera.depthCompensation)) // Adjust for depth projection.
-
-        Face(rect, sortDepth + sortDepthAdjustment, outline, fill, image)
-      }
-
-      val topFace = makeFace(worldOrigin = r.box.vertex(LEFT, BACK, TOP),
-                             height = size.y,
-                             outline = r.outline,
-                             fill = r.fill,
-                             image = r.topImage,
-                             sortDepthAdjustment = topFaceDepthPriorityNudge)
-
-      val frontFace = makeFace(worldOrigin = r.box.vertex(LEFT, FRONT, TOP),
-                               height = size.z,
-                               outline = r.outline map (_.darker.darker.darker),
-                               fill = r.fill map (_.darker.darker),
-                               image = r.frontImage)
-
-      Seq(topFace, frontFace)
-    }
-
-    zoneDrawables flatMap toFaces
+    Face(compensatedForProjection, sortDepth, outline, fill, image)
   }
+
+
+  def apply(d: BoxDrawable, cam: OrthographicCamera) = {
+
+    val sortDepthZFactor = 0.1
+
+    val sortOrigin = d.box.vertex(LEFT, BACK, TOP)
+    val sortDepth = sortOrigin.y + (sortOrigin.z * sortDepthZFactor)
+
+    val projectedSize = cam.project(d.box.size)
+
+    val topFace = makeFace(projectedOrigin = cam.project(d.box.vertex(LEFT, BACK, TOP)),
+                           width = projectedSize.x,
+                           height = projectedSize.y,
+                           outline = d.outline,
+                           fill = d.fill,
+                           image = d.topImage,
+                           sortDepth = sortDepth + 0.00001, // Top faces receive priority over front
+                                                            // at same depth, prevents Z-fighting
+                                                            // between faces of Y-adjacent boxes.
+                           cam = cam)
+
+    val frontFace = makeFace(projectedOrigin = cam.project(d.box.vertex(LEFT, FRONT, TOP)),
+                             width = projectedSize.x,
+                             height = projectedSize.z,
+                             outline = d.outline map (_.darker.darker.darker),
+                             fill = d.fill map (_.darker.darker),
+                             image = d.frontImage,
+                             sortDepth = sortDepth + projectedSize.y,
+                             cam = cam)
+
+    Seq(topFace, frontFace)
+  }
+
+  def apply(drawables: Seq[BoxDrawable], cam: OrthographicCamera): Seq[Face] =
+    drawables flatMap (d => apply(d, cam))
 }
